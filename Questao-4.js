70c15a71282350792b212ed3dda79ceea618c591
Claudio Calipi <165864381+claudioNinja10@users.noreply.github.com>
2024-09-08 23:22:56 -0300

    Update Questao-4.js

---
+++ Questao-4.js
@@ -0,0 +1,9 @@
+function questao(){
+    let idade_questao = Number(prompt('Digite a sua idade: '))
+
+    if (idade_questao>=18){
+        alert("É maior de idade.")
+    }else{
+        alert("É menor de idade.")
+    }
+}
