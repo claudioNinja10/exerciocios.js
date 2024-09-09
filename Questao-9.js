ddf52d851e55ffd3e4e3b01c4366cda8b191224c
Claudio Calipi <165864381+claudioNinja10@users.noreply.github.com>
2024-09-08 23:24:48 -0300

    Update Questao-9.js

---
+++ Questao-9.js
@@ -0,0 +1,9 @@
+function questao9(){
+    function soma(x,y){
+        let soma_questao9 = x + y
+        console.log(soma_ex9)
+    }
+    let numero_1_questao9 = Number(prompt('Digite um número: '))
+    let numero_2_questao9 = Number(prompt('Digite outro número: '))
+    soma(numero_1_questao9, numero_2_questao9)
+}
